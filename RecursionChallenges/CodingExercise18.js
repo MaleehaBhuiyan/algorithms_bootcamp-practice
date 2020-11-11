f5cfa8dc2ea4cd97b3989573e493677cec149b6d
MaleehaBhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-11-10 20:53:13 -0500

    Code challenge 18 complete

--- RecursionChallenges/CodingExercise18.js
+++ RecursionChallenges/CodingExercise18.js
@@ -1,16 +1,13 @@
 //Write a recursive function called flatten which accepts an array and returns a new array with all values flattened 
 
-function flatten(arr){
-    var flatArr = arr.flat()
-    canFlatten(arr, flatArr)
-}
-
-function canFlatten(arr, flatArr){
-    if(arr != flatArr){
-        
+function flatten(oldArr){
+    var newArr = []
+    for(var i = 0; i < oldArr.length; i++){
+        if(Array.isArray(oldArr[i])){
+            newArr = newArr.concat(flatten(oldArr[i]))
+        } else {
+            newArr.push(oldArr[i])
+        }
     }
+    return newArr
 }
-
-
-
-flatten([[1],[2],[3]])
