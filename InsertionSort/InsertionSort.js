876ba93f96e66938e23e834b1a724821a78e198d
MaleehaBhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-11-13 10:59:55 -0500

    Insertion Sort Complete

--- InsertionSort/InsertionSort.js
+++ InsertionSort/InsertionSort.js
@@ -1,9 +1,17 @@
 function insertionSort(arr){
     for(var i = 1; i < arr.length; i++){
         var currentVal = arr[i]
-        for(var j = i - 1; j >= 0  )
+        for(var j = i - 1; j >= 0 && arr[j] > currentVal; j--){
+            arr[j+1] = arr[j]
+        }
+        arr[j+1] = currentVal
     }
     return arr;
 }
 
 insertionSort([2,1,9,76,4])
+
+/* 
+O(n^2)
+But if data is almost all sorted O(n)
+*/
